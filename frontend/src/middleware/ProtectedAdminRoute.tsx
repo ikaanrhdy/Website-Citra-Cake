f4670e1006f082d411f7c5abec7903a73d9279b9
Ika Nur Hidayati <hidayatiikanur58@gmail.com>
2026-07-18 18:57:59 +0700

    fix: update not found

--- frontend/src/middleware/ProtectedAdminRoute.tsx
+++ frontend/src/middleware/ProtectedAdminRoute.tsx
@@ -4,14 +4,16 @@ import { Navigate, Outlet } from "react-router";
 export default function ProtectedAdminRoute() {
   const { isAuthenticated, isLoading, isTokenExpired, logout } = useAuthStore();
 
+  // Memastikan sistem dalam kondisi siap (tidak sedang memuat)
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
+  // Validasi autentikasi dan masa berlaku token
   if (!isAuthenticated || isTokenExpired()) {
     if (isAuthenticated) logout(); // bersihin state kalau ternyata token udah basi
     return <Navigate to="/login-admin" replace />;
   }
 
+  // Mengizinkan akses jika validasi berhasil
   return <Outlet />;
 }
